ee0d49a65e52f04631add728ba8961f388ba908d
hsyu <g0222@evertrust.com.tw>
2021-01-15 22:47:41 +0800

    加入專案檔案。

---
+++ Model/ParameterDataModel.cs
@@ -0,0 +1,55 @@
+﻿namespace SeleniumConsoleApp.Model
+{
+    public class ParameterDataModel
+    {
+        /// <summary>
+        /// 多久檢查一次
+        /// </summary>
+        public int Timer { get; set; }
+
+        /// <summary>
+        /// 簽到在甚麼時間開始
+        /// </summary>
+        public int SignInAt { get; set; }
+
+        /// <summary>
+        /// 簽到在甚麼時間之前
+        /// </summary>
+        public int SignInBefore { get; set; }
+
+        /// <summary>
+        /// 簽退在甚麼時間開始
+        /// </summary>
+        public int SignOutAt { get; set; }
+
+        /// <summary>
+        /// 簽退在甚麼時間之前
+        /// </summary>
+        public int SignOutBefore { get; set; }
+
+        /// <summary>
+        /// 每周哪幾天要執行
+        /// </summary>
+        public int[] DayOfWeek { get; set; }
+
+        /// <summary>
+        /// 生日
+        /// </summary>
+        public string Birthday { get; set; }
+
+        /// <summary>
+        /// 到職日
+        /// </summary>
+        public string JobDay { get; set; }
+
+        /// <summary>
+        /// EMail
+        /// </summary>
+        public string EMail { get; set; }
+
+        /// <summary>
+        /// 身分證字號
+        /// </summary>
+        public string Id { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using Newtonsoft.Json.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumConsoleApp.Model;
+using Timer = System.Timers.Timer;
+
+namespace SeleniumConsoleApp
+{
+    class Program
+    {
+        private readonly static string _url = "";
+        private static Timer _timer;
+        private static ParameterDataModel _parameter;
+
+        static void Main(string[] args)
+        {
+            _parameter = JObject.Parse(
+                           File.ReadAllText(@"parameter.json")).ToObject<ParameterDataModel>();
+            _timer = new Timer(TimeSpan.FromMinutes(_parameter.Timer).TotalMilliseconds);
+
+            TriggerEven();
+
+            // Hook up the Elapsed event for the timer. 
+            _timer.Elapsed += (sender, e) => TriggerEven();
+
+            // Have the timer fire repeated events (true is the default)
+            _timer.AutoReset = true;
+
+            // Start the timer
+            _timer.Enabled = true;
+
+            Console.WriteLine("Press the Enter key to exit the program at any time... ");
+            Console.ReadLine();
+        }
+
+        private static void TriggerEven()
+        {
+            // 08點檢查簽到
+            if (DateTime.Now.Hour.Equals(8) &&
+                (DateTime.Now.Minute >= _parameter.SignInAt && DateTime.Now.Minute <= _parameter.SignInBefore) &&
+                _parameter.DayOfWeek.Contains((int)DateTime.Now.DayOfWeek))
+            {
+                SignIn();
+            }
+
+            // 18點檢查簽退
+            if (DateTime.Now.Hour.Equals(18) &&
+                (DateTime.Now.Minute >= _parameter.SignOutAt && DateTime.Now.Minute <= _parameter.SignOutBefore) &&
+                _parameter.DayOfWeek.Contains((int)DateTime.Now.DayOfWeek))
+            {
+                SignOut();
+            }
+            Console.WriteLine("The Elapsed event was raised at {0}", DateTime.Now);
+        }
+
+        /// <summary>
+        /// 簽到
+        /// </summary>
+        private static void SignIn()
+        {
+            using (var driver = new ChromeDriver("."))
+            {
+                //開啟網頁
+                driver.Navigate().GoToUrl(_url);
+                Thread.Sleep(2000);
+
+                //點擊執行
+                var submitButton = driver.FindElementsByCssSelector("button.checkin");
+                Thread.Sleep(2000);
+                submitButton.FirstOrDefault().Click();
+                Thread.Sleep(2000);
+
+                Verify(driver);
+
+                driver.Navigate().GoToUrl(_url);
+                driver.Quit();
+            }
+        }
+
+        /// <summary>
+        /// 簽退
+        /// </summary>
+        private static void SignOut()
+        {
+            using (var driver = new ChromeDriver("."))
+            {
+                //開啟網頁
+                driver.Navigate().GoToUrl(_url);
+                Thread.Sleep(2000);
+
+                //點擊執行
+                var submitButton = driver.FindElementsByCssSelector("button.checkout");
+                Thread.Sleep(2000);
+                submitButton.FirstOrDefault().Click();
+                Thread.Sleep(2000);
+
+                //如果有重複點選簽退
+                try
+                {
+                    var confirm_win = driver.SwitchTo().Alert();
+                    confirm_win.Accept();
+                }
+                catch (System.Exception ex)
+                {
+
+                }
+
+                Thread.Sleep(2000);
+
+                Verify(driver);
+
+                driver.Navigate().GoToUrl(_url);
+                driver.Quit();
+            }
+        }
+
+        /// <summary>
+        /// 解決驗證彈跳
+        /// </summary>
+        /// <param name="driver">The driver.</param>
+        private static void Verify(ChromeDriver driver)
+        {
+            driver.SwitchTo().Frame(driver.FindElement(By.TagName("iframe")));
+
+            Thread.Sleep(2000);
+
+            //回答問題
+            var labQuestion = driver.FindElement(By.XPath(@"//*[@id='labQuestion']")).Text;
+            var answer = string.Empty;
+
+            if (labQuestion.Contains("生日"))
+            {
+                answer = _parameter.Birthday;
+            }
+            else if (labQuestion.Contains("到職日"))
+            {
+                answer = _parameter.JobDay;
+            }
+            else if (labQuestion.Contains("外部EMAIL"))
+            {
+                answer = _parameter.EMail;
+            }
+            else if (labQuestion.Contains("身份證號"))
+            {
+                answer = _parameter.Id;
+            }
+
+            var txtAnswer = driver.FindElement(By.XPath(@"//*[@id='txtAnswer']"));
+            Thread.Sleep(2000);
+            txtAnswer.SendKeys(answer);
+            Thread.Sleep(2000);
+
+            var btnSave = driver.FindElement(By.XPath(@"//*[@id='btnSave']"));
+            Thread.Sleep(2000);
+            btnSave.Click();
+            Thread.Sleep(2000);
+
+            //如果有重複點選簽退
+            var check_result = driver.SwitchTo().Alert();
+            check_result.Accept();
+            Thread.Sleep(2000);
+        }
+    }
+}
